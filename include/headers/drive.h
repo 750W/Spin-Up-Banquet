6da57ed33809e074b5e5c83d01455ee1aa749b4b
AjayAvasi <79114792+AjayAvasi@users.noreply.github.com>
2023-03-25 12:31:07 -0400

    Adding All Files

---
+++ include/headers/drive.h
@@ -0,0 +1,9 @@
+#include "main.h"
+
+extern pros::Motor left_mtr;
+extern pros::Motor left_mtr_2;
+extern pros::Motor left_mtr_3;
+
+extern pros::Motor right_mtr;
+extern pros::Motor right_mtr_2;
+extern pros::Motor right_mtr_3;
---
+++ src/subsystems/drive.cpp
@@ -0,0 +1,11 @@
+#include "main.h"
+
+
+
+pros::Motor left_mtr(1,pros::E_MOTOR_GEAR_BLUE,true);
+pros::Motor left_mtr_2(2,pros::E_MOTOR_GEAR_BLUE,true);
+pros::Motor left_mtr_3(3,pros::E_MOTOR_GEAR_BLUE,true);
+
+pros::Motor right_mtr(4,pros::E_MOTOR_GEAR_BLUE);
+pros::Motor right_mtr_2(5,pros::E_MOTOR_GEAR_BLUE);
+pros::Motor right_mtr_3(6,pros::E_MOTOR_GEAR_BLUE);
